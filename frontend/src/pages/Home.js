a953afaa04e6cf1da75b752042fdf2b92c1a23d2
Riley Arthur <rlarthur@ucsc.edu>
2025-10-29 08:54:52 -0700

    updated landing page and styling

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,28 @@
 import { Link } from 'react-router-dom';
+
 function Home() {
+  const diningHalls = [
+    'John R. Lewis & College Nine Dining Hall',
+    'Cowell & Stevenson Dining Hall',
+    'Crown & Merrill Dining Hall',
+    'Porter & Kresge Dining Hall',
+    'Rachel Carson & Oakes Dining Hall'
+  ];
+
   return (
     <div className="home-container">
-      <h1 className="home-title">Welcome to the UCSC Dining Menu!</h1>
-      <Link to="/dining-halls"><button className="choose-dining-hall-button">Choose Dining Hall</button></Link>
+      <br />
+      <h1 className="dining-halls-page-title">Dining Halls</h1>
+      
+      <div className="dining-halls-grid">
+        {diningHalls.map((hall, index) => (
+          <Link key={index} to={`/menu/${encodeURIComponent(hall)}`}>
+            <div className="dining-hall-card">
+              <h2>{hall.replace(' Dining Hall', '').replace(' & ', '/')}</h2>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
