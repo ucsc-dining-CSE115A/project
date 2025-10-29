d3ba7d05ddbc92309cd83055aece2dc1c4b7355e
Riley Arthur <rlarthur@ucsc.edu>
2025-10-29 08:58:30 -0700

    Worked on landing page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,23 +2,40 @@ import { Link } from 'react-router-dom';
 
 function Home() {
   const diningHalls = [
-    'John R. Lewis & College Nine Dining Hall',
-    'Cowell & Stevenson Dining Hall',
-    'Crown & Merrill Dining Hall',
-    'Porter & Kresge Dining Hall',
-    'Rachel Carson & Oakes Dining Hall'
+    'JRL/C9',
+    'Cowell/Stevenson',
+    'Crown/Merrill',
+    'Porter/Kresge',
+    'RC/Oakes'
   ];
+  const cafes = [
+    'Oakes Cafe',
+    'Global Village Cafe',
+    'Owl\'s Nest Cafe',
+    'Stevenson Coffee House',
+    'Perk Coffee Bar',
+    'Banana Joe\'s Cafe'
+
+  ]
 
   return (
     <div className="home-container">
-      <br />
       <h1 className="dining-halls-page-title">Dining Halls</h1>
-      
       <div className="dining-halls-grid">
         {diningHalls.map((hall, index) => (
-          <Link key={index} to={`/menu/${encodeURIComponent(hall)}`}>
+          <Link key={index} to={`/menu/${hall}`}>
             <div className="dining-hall-card">
-              <h2>{hall.replace(' Dining Hall', '').replace(' & ', '/')}</h2>
+              <h2>{hall}</h2>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <div className="dining-halls-page-title">Cafes</div>
+      <div className="dining-halls-grid">
+        {cafes.map((cafe, index) => (
+          <Link key={index} to={`/menu/${cafe}`}>
+            <div className="dining-hall-card">
+              <h2>{cafe}</h2>
             </div>
           </Link>
         ))}
